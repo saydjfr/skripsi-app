171608c054bcb88000356b3544661b5ad5521cc5
saydjfr <sundek121@gmail.com>
2024-12-22 23:35:27 +0700

    Modify Order Resource Widgets

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Forms\Components\Tabs;
+use Filament\Resources\Components\Tab;
+use Filament\Resources\Components\Tab as ComponentsTab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -14,6 +18,23 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return[
+            OrderStats::class
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return[
+            null=> Tab::make('All'),
+            'New'=>Tab::make()->query(fn($query)=>$query->where('status','new')),
+            'Processing'=>Tab::make()->query(fn($query)=>$query->where('status','processing')),
+            'Completed'=>Tab::make()->query(fn($query)=>$query->where('status','completed'))
         ];
     }
 }
